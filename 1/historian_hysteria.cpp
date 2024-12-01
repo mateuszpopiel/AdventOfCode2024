0529bb7f2fd3ac39f1ccfdd2084afee36f6a8d86
Mateusz Popiel <popielmateusz12@gmail.com>
2024-12-02 00:15:15 +0100

    Finish Day 1

---
+++ 1/historian_hysteria.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+
+using LocationId = unsigned int;
+
+unsigned int get_total_distance(const std::pair<std::vector<LocationId>, std::vector<LocationId>> &lists_of_locations) {
+    if (lists_of_locations.first.size() != lists_of_locations.second.size()) {
+        throw std::invalid_argument("Lists must have the same size");
+    }
+
+    auto left_list = lists_of_locations.first;
+    auto right_list = lists_of_locations.second;
+
+    std::sort(left_list.begin(), left_list.end());
+    std::sort(right_list.begin(), right_list.end());
+
+    unsigned int distance = 0;
+    for (size_t i = 0; i < left_list.size(); ++i) {
+        distance += std::abs(static_cast<int>(left_list[i]) - static_cast<int>(right_list[i]));
+    }
+    return distance;
+}
+
+int main() {
+    std::ifstream file("data.txt");
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open file." << std::endl;
+        return 1;
+    }
+
+    std::vector<LocationId> left_list;
+    std::vector<LocationId> right_list;
+
+    LocationId left, right;
+    while (file >> left >> right) {
+        left_list.push_back(left);
+        right_list.push_back(right);
+    }
+
+    try {
+        std::cout << get_total_distance(std::make_pair(left_list, right_list)) << std::endl;
+    } catch (const std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
