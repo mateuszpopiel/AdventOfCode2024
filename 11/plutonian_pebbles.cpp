22412268705eb5b44970ff516a224261ef67151b
Mateusz Popiel <popielmateusz12@gmail.com>
2024-12-11 17:09:52 +0100

    Refactor day 11

--- 11/plutonian_pebbles.cpp
+++ 11/plutonian_pebbles.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <string>
 #include <string_view>
 #include <sstream>
 #include <algorithm>
 #include <array>
+#include <unordered_map>
+#include <numeric>
+#include <cstdint>
 #include <cmath>
-#include <chrono>
 
 static constexpr std::string_view filename = "data.txt";
-static constexpr size_t max_vector_size = 1'000'000;
 
 auto open_file(const std::string_view filename) {
   std::ifstream file(filename.data());
@@ -20,27 +20,20 @@ auto open_file(const std::string_view filename) {
   return file;
 }
 
-auto file_to_line(std::ifstream &file) {
+auto file_to_line(std::ifstream &file) -> std::unordered_map<uint64_t, uint64_t> {
   std::string line_as_string;
-  std::vector<size_t> line_as_vector_of_numbers;
+  std::unordered_map<uint64_t, uint64_t> line_as_map;
   std::getline(file, line_as_string);
   std::istringstream iss(line_as_string);
-  size_t number;
+  uint64_t number;
   while (iss >> number) {
-    line_as_vector_of_numbers.push_back(number);
+    line_as_map[number]++;
   }
-  return line_as_vector_of_numbers;
+  return line_as_map;
 }
 
-constexpr auto rule_1(const size_t stone_number) -> size_t {
-  if (stone_number != 0) {
-    throw std::runtime_error("The first rule applied incorrectly");
-  }
-  return 1;
-}
-
-constexpr auto get_num_of_digits(size_t number) -> size_t {
-  size_t digits = 0;
+constexpr auto get_num_of_digits(uint64_t number) -> uint64_t {
+  uint64_t digits = 0;
   while (number > 0) {
     number /= 10;
     ++digits;
@@ -48,91 +41,47 @@ constexpr auto get_num_of_digits(size_t number) -> size_t {
   return digits;
 }
 
-constexpr auto has_even_num_of_digits(const size_t number) -> bool {
-  return get_num_of_digits(number) % 2 == 0;
-}
-
-constexpr auto split_number_digits(const size_t number) -> std::array<size_t, 2> {
-  const size_t number_of_digits = get_num_of_digits(number);
-  const size_t first_number = number / std::pow(10, number_of_digits / 2);
-  const size_t second_number = number % static_cast<size_t>(std::pow(10, number_of_digits / 2));
+constexpr auto split_number_digits(const uint64_t number) -> std::array<uint64_t, 2> {
+  const uint64_t number_of_digits = get_num_of_digits(number);
+  const uint64_t first_number = number / std::pow(10, number_of_digits / 2);
+  const uint64_t second_number = number % static_cast<uint64_t>(std::pow(10, number_of_digits / 2));
   return {first_number, second_number};
 }
 
-constexpr auto rule_2(const size_t stone_number) {
-  if (!has_even_num_of_digits(stone_number)) {
-    throw std::runtime_error("The second rule applied incorrectly");
-  }
-  return split_number_digits(stone_number);
-}
-
-constexpr auto rule_3(const size_t stone_number) {
-  return stone_number * 2024;
-}
-
-auto blink(const std::vector<size_t> &line) {
-  std::vector<size_t> new_line {};
-  new_line.reserve(max_vector_size);
-  for (const auto number : line) {
-    if (number == 0) {
-      new_line.push_back(rule_1(number));
+auto blink(const std::unordered_map<uint64_t, uint64_t> &line) -> std::unordered_map<uint64_t, uint64_t> {
+  std::unordered_map<uint64_t, uint64_t> new_line;
+  for (const auto &[stone, count] : line) {
+    if (stone == 0) {
+      new_line[1] += count;
       continue;
     }
-    if (has_even_num_of_digits(number)) {
-      const auto [first_number, second_number] = rule_2(number);
-      new_line.insert(new_line.end(), {first_number, second_number});
+    if (get_num_of_digits(stone) % 2 == 0) {
+      const auto [first_part, second_part] = split_number_digits(stone);
+      new_line[first_part] += count;
+      new_line[second_part] += count;
       continue;
     }
-    new_line.push_back(rule_3(number));
+    new_line[stone * 2024] += count;
   }
   return new_line;
 }
 
-auto split_vector_into_chunks(const std::vector<size_t> &line, const size_t chunk_size) {
-  std::vector<std::vector<size_t>> chunks;
-  chunks.reserve(max_vector_size);
-  for (size_t i = 0; i < line.size(); i += chunk_size) {
-    const auto begin = line.begin() + i;
-    const auto potential_end_of_chunk_index = line.begin() + i + chunk_size;
-    const auto end = std::min(potential_end_of_chunk_index, line.end());
-    chunks.push_back(std::vector<size_t>(begin, end));
-  }
-  return chunks;
-}
-
-auto blink_n_times(const std::vector<size_t> &line, const size_t n) -> size_t;
-
-auto solve_in_chunks(const std::vector<size_t> &line, const size_t chunk_size, const size_t n) -> size_t {
-  if (n == 0) {
-    return line.size();
-  }
-  size_t num_of_stones = 0;
-  const auto chunks = split_vector_into_chunks(line, chunk_size);
-  for (const auto &chunk : chunks) {
-    num_of_stones += blink_n_times(chunk, n);
-  }
-  return num_of_stones;
-}
-
-auto blink_n_times(const std::vector<size_t> &line, const size_t n) -> size_t {
-  std::vector<size_t> new_line = line;
-  for (size_t i = 0; i < n; ++i) {
+auto blink_n_times(const std::unordered_map<uint64_t, uint64_t> &line, const uint64_t n) -> uint64_t {
+  auto new_line = line;
+  for (uint64_t i = 0; i < n; ++i) {
     new_line = blink(new_line);
-    if (new_line.size() > max_vector_size) {
-      return solve_in_chunks(new_line, max_vector_size, n - i - 1);
-    }
   }
-  return new_line.size();
+
+  return std::accumulate(new_line.begin(), new_line.end(), 0, [](const uint64_t sum, const auto &pair) {
+    return sum + pair.second;
+  });
 }
 
 int main() {
   auto file = open_file(filename);
-  const auto line_of_stones = file_to_line(file);
-  const auto before = std::chrono::high_resolution_clock::now();
+  auto line_of_stones = file_to_line(file);
+
   std::cout << blink_n_times(line_of_stones, 25) << '\n'; // 199753
-  const auto after = std::chrono::high_resolution_clock::now();
-  std::cout << "Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(after - before).count() << "ms\n";
-  std::cout << blink_n_times(line_of_stones, 75) << '\n';
-  std::cout << "Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - after).count() << "ms\n";
+  std::cout << blink_n_times(line_of_stones, 75) << '\n'; // 239413123020116
   return 0;
 }
