a95d9afd9f3554c61c447c43ce5aa36fdb83a629
Mateusz Popiel <popielmateusz12@gmail.com>
2024-12-15 20:49:27 +0100

    Add solution to tests

--- 15/tests/test_day_15.cpp
+++ 15/tests/test_day_15.cpp
@@ -2,4 +2,4 @@
 #include <gtest/gtest.h>
 
 TEST(Day15, Part1) { EXPECT_EQ(1568399, solve(false)); }
-TEST(Day15, Part2) { EXPECT_EQ(0, solve(true)); }
+TEST(Day15, Part2) { EXPECT_EQ(1575877, solve(true)); }
