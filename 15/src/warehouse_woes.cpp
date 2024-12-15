ecb138f34ef7b9ce4f87e389bdd3c0103d69cb0e
Mateusz Popiel <popielmateusz12@gmail.com>
2024-12-15 20:48:37 +0100

    Refactor day 15 further

--- 15/src/warehouse_woes.cpp
+++ 15/src/warehouse_woes.cpp
@@ -1,6 +1,8 @@
 #include "warehouse_woes.hpp"
 #include "file_helpers_day_15.hpp"
 #include <algorithm>
+#include <functional>
+#include <iostream>
 #include <numeric>
 #include <stdexcept>
 #include <string>
@@ -9,19 +11,67 @@
 static const std::unordered_map<char, Coordinates<long>> movements{
     {'v', {1, 0}}, {'^', {-1, 0}}, {'>', {0, 1}}, {'<', {0, -1}}};
 
+bool can_move(const std::vector<Coordinates<long>> &positions, const std::unordered_map<Coordinates<long>, char> &map,
+              const char direction) {
+  switch (direction) {
+  case 'v':
+    return std::all_of(positions.begin(), positions.end(),
+                       [&map](const auto &pos) { return map.at(pos + movements.at('v')) == '.'; });
+  case '^':
+    return std::all_of(positions.begin(), positions.end(),
+                       [&map](const auto &pos) { return map.at(pos + movements.at('^')) == '.'; });
+  case '>':
+    return map.at(Coordinates<long>{positions.front().x, std::max(positions.front().y, positions.back().y) + 1}) == '.';
+  case '<':
+    return map.at(Coordinates<long>{positions.front().x, std::min(positions.front().y, positions.back().y) - 1}) == '.';
+  }
+  return false;
+}
+
+void move_box(const std::vector<Coordinates<long>> &box, const char direction,
+              std::unordered_map<Coordinates<long>, char> &map) {
+  if (direction == '<') {
+    std::for_each(box.begin(), box.end(), [&map, direction](const auto &pos) {
+      map.at(pos + movements.at(direction)) = map.at(pos);
+      map.at(pos) = '.';
+    });
+    return;
+  }
+  // In case of 'V' or '^' order doesn't really matter, it had to be inverted for '>'
+  std::for_each(box.rbegin(), box.rend(), [&map, direction](const auto &pos) {
+    map.at(pos + movements.at(direction)) = map.at(pos);
+    map.at(pos) = '.';
+  });
+}
+
 Coordinates<long> move(const Coordinates<long> &pos, const char direction,
-                       std::unordered_map<Coordinates<long>, char> &map) {
+                       std::unordered_map<Coordinates<long>, char> &map, const bool part_2) {
   auto next_pos = pos + movements.at(direction);
   if (map.at(next_pos) == '#') {
     return pos;
   }
-  if (map.at(next_pos) == 'O') {
-    move(next_pos, direction, map);
+  if (map.at(next_pos) == 'O' || map.at(next_pos) == '[' || map.at(next_pos) == ']') {
+    move(next_pos, direction, map, part_2);
   }
-  if (map.at(next_pos) == '.') {
+  if (map.at(pos) == '@' && map.at(next_pos) == '.') {
     map.at(next_pos) = map.at(pos);
     map.at(pos) = '.';
     return next_pos;
+  }
+  std::vector<Coordinates<long>> box{};
+  if (!part_2) {
+    box.push_back(pos);
+  } else {
+    if (map.at(pos) == '[') {
+      box.push_back(pos);
+      box.push_back(pos + movements.at('>'));
+    } else {
+      box.push_back(pos + movements.at('<'));
+      box.push_back(pos);
+    }
+  }
+  if (can_move(box, map, direction)) {
+    move_box(box, direction, map);
   }
   return pos;
 }
@@ -34,10 +84,11 @@ Coordinates<long> find_robot(const std::unordered_map<Coordinates<long>, char> &
   return robot_iter->first;
 }
 
-void simulate_robot_movement(std::unordered_map<Coordinates<long>, char> &map, const std::string &movement) {
+void simulate_robot_movement(std::unordered_map<Coordinates<long>, char> &map, const std::string &movement,
+                             const bool part_2) {
   auto robot = find_robot(map);
   for (const auto &direction : movement) {
-    robot = move(robot, direction, map);
+    robot = move(robot, direction, map, part_2);
   }
 }
 
@@ -53,6 +104,6 @@ long compute_sum_of_boxes_gps(const std::unordered_map<Coordinates<long>, char> 
 
 ull solve(const bool part_2) {
   auto [map, movement] = get_map_and_movement_from_file("data.txt", part_2);
-  simulate_robot_movement(map, movement);
+  simulate_robot_movement(map, movement, part_2);
   return static_cast<ull>(compute_sum_of_boxes_gps(map, part_2));
 }
--- lib/inc/coordinates.hpp
+++ lib/inc/coordinates.hpp
@@ -1,8 +1,15 @@
 #pragma once
 
 template <class T> struct Coordinates {
-  T x;
-  T y;
+  T x, y;
+
+  constexpr Coordinates() : x(0), y(0) {}
+  constexpr Coordinates(const T &x, const T &y) : x(x), y(y) {}
+
+  constexpr Coordinates(const Coordinates &other) = default;
+  constexpr Coordinates(Coordinates &&other) noexcept = default;
+  constexpr Coordinates &operator=(const Coordinates &other) = default;
+  constexpr Coordinates &operator=(Coordinates &&other) noexcept = default;
 
   constexpr Coordinates operator+(const Coordinates &other) const { return {x + other.x, y + other.y}; }
   constexpr Coordinates operator-(const Coordinates &other) const { return {x - other.x, y - other.y}; }
@@ -11,6 +18,11 @@ template <class T> struct Coordinates {
   constexpr Coordinates operator/(const T &divisor) const { return {x / divisor, y / divisor}; }
   constexpr Coordinates operator%(const Coordinates &modulus) const { return {x % modulus.x, y % modulus.y}; }
   constexpr Coordinates operator%(const T &modulus) const { return {x % modulus, y % modulus}; }
+  constexpr Coordinates &operator+=(const Coordinates &other) {
+    this->x += other.x;
+    this->y += other.y;
+    return *this;
+  }
 
   constexpr bool operator==(const Coordinates &other) const { return x == other.x && y == other.y; }
   constexpr bool operator!=(const Coordinates &other) const { return !(*this == other); }
